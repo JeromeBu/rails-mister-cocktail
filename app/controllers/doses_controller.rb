498a021d4f02934795d061180bfee3d6ec7eb402
Jerome BURKARD <jerome.burkard@gadz.org>
2016-11-17 14:48:59 +0100

    dose controller ongoing

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,34 @@
 class DosesController < ApplicationController
+
+  before_action :current_cocktail, only: [:new, :create]
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    @ingredient = Ingredient.find(params[:dose][:ingredient].to_i)
+    @dose.ingredient = @ingredient
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def current_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description ,:ingredient_id, :cocktail_id)
+  end
 end
